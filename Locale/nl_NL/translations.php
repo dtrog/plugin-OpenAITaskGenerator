882297bc0ebc34849c99b97084d9f0b828927a69
Damien Trog <dtrog@me.com>
2024-11-05 17:07:33 +0100

    typo

--- Locale/nl_NL/translations.php
+++ Locale/nl_NL/translations.php
@@ -2,7 +2,7 @@
 return array(
     "Adjust the settings below to set the api key for OpenAI" => "Configureer de instellingen hieronder voor OpenAI",
     "AI Task Generator" => "AI Taken Generator",
-    "Create Tasks'" => "Maak Taken",
+    "Create Tasks" => "Maak Taken",
     "Generate" => "Genereer",
     "Generate Tasks from Goal" => "Genereer taken voor een doel",
     "Enter your project goal" => "Geef je doel in",
